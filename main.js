c9da53085013d09ce9dad117f89812a42b112e77
H4wk <quasand89@gmail.com>
2022-10-17 00:31:13 +0200

    improvements, refactoring

--- main.js
+++ main.js
@@ -1,72 +1,74 @@
+"use strict";
+
 (() => {
   class Book {
     constructor(author, title, pages, read) {
-      return { author, title, pages, read };
+      this.author = author;
+      this.title = title;
+      this.pages = pages;
+      this.read = read;
     }
+
     changeReadStatus() {
-      if (this.read === "yes") this.read = "no";
-      else if (this.read === "no") this.read = "yes";
+      this.read = !this.read;
     }
   }
 
-  function addBookToLibrary(author, title, pages, read) {
-    myLibrary.push(new Book(author, title, pages, read));
+  class Library {
+    constructor(books) {
+      this.books = books;
+    }
+
+    addBook(author, title, pages, read) {
+      if (!this.alreadyExist(title, author)) {
+        this.books.push(new Book(author, title, pages, read));
+      }
+    }
+
+    removeBook(idx) {
+      this.books.splice(idx, 1);
+    }
+
+    alreadyExist(title, author) {
+      return this.books.some((book) => {
+        return book.title === title && book.author === author;
+      });
+    }
+
+    size() {
+      return this.books.length;
+    }
   }
-
-  /*
-  function createForm() {
-    const form = document.createElement("form");
-
-    const title = document.createElement("input");
-    title.setAttribute("type", "text");
-    title.setAttribute("id", "title");
-    title.setAttribute("placeholder", "Title");
-
-    const author = document.createElement("input");
-    author.setAttribute("type", "text");
-    author.setAttribute("id", "author");
-    author.setAttribute("placeholder", "Author");
-
-    const pages = document.createElement("input");
-    pages.setAttribute("placeholder", "Number of pages");
-    pages.setAttribute("id", "pages");
-    pages.setAttribute("type", "number");
-
-    const read = document.createElement("input");
-    read.setAttribute("type", "text");
-    read.setAttribute("id", "read");
-    read.setAttribute("placeholder", "read? yes or no");
-    read.setAttribute("pattern", "^(yes|no)$");
-
-    const addButton = document.createElement("button");
-    addButton.setAttribute("type", "button");
-    addButton.innerText = "ADD";
-    addButton.addEventListener("click", addBook);
-
-    form.appendChild(title);
-    form.appendChild(author);
-    form.appendChild(pages);
-    form.appendChild(read);
-    form.appendChild(addButton);
-
-    button.parentNode.insertBefore(form, button.nextSibling);
-    this.disabled = true;
-  }
-  */
 
   function addBook() {
     /**
      * Get values from the input form and append a new book.
      */
-    const cardsContainer = document.querySelector(".card-container");
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
-    const read = document.getElementById("read").value;
+    const read = document.getElementById("read").checked;
+    // TODO: change this
+    const errorDiv = document.querySelector(".error");
 
-    addBookToLibrary(title, author, pages, read);
-    const book = myLibrary[myLibrary.length - 1];
-    cardsContainer.appendChild(prepareCard(book, myLibrary.length - 1));
+    if (
+      title.length === 0 ||
+      author.length === 0 ||
+      pages.length === 0 ||
+      parseInt(pages) <= 0
+    ) {
+      // Incorrect form values.
+      return;
+    } else if (myLibrary.alreadyExist(title, author)) {
+      errorDiv.classList.add("show");
+    } else {
+      myLibrary.addBook(title, author, pages, read);
+      const book = myLibrary.books[myLibrary.size() - 1];
+      cardsContainer.appendChild(prepareCard(book, myLibrary.size() - 1));
+      modalContainer.classList.remove("show");
+      errorDiv.classList.remove("show");
+      form.reset();
+    }
   }
 
   function prepareCard(book, idx) {
@@ -79,11 +81,15 @@
     cardTitle.innerText = book.title;
     card.appendChild(cardTitle);
 
+    card.appendChild(document.createElement("hr"));
+
     // card's author
     const cardAuthor = document.createElement("div");
     cardAuthor.classList.add("card-author");
     cardAuthor.innerText = book.author;
     card.appendChild(cardAuthor);
+
+    card.appendChild(document.createElement("hr"));
 
     // card's pages
     const cardPages = document.createElement("div");
@@ -93,9 +99,9 @@
 
     const readButton = document.createElement("button");
     readButton.setAttribute("type", "button");
-    readButton.innerText = book.read === "yes" ? "Read" : "Not read";
+    readButton.innerText = book.read ? "Read" : "Not read";
     readButton.classList.add("card-read");
-    readButton.classList.add(book.read === "yes" ? "read" : "unread");
+    readButton.classList.add(book.read ? "read" : "unread");
     readButton.addEventListener("click", () => {
       if (readButton.classList.contains("read")) {
         readButton.classList.remove("read");
@@ -126,50 +132,52 @@
     /**
      * Prepare the initial state of the cards.
      */
-    addBookToLibrary("George Orwell", "1984", 350, "yes");
-    addBookToLibrary("F. Scott Fitzgerald", "The Great Gatsby", 250, "yes");
-    addBookToLibrary("Bret Easton Ellis", "American Psycho", 400, "yes");
-    addBookToLibrary("Plato", "Republic", 206, "no");
+    myLibrary.addBook("George Orwell", "1984", 350, true);
+    myLibrary.addBook("F. Scott Fitzgerald", "The Great Gatsby", 250, true);
+    myLibrary.addBook("Bret Easton Ellis", "American Psycho", 400, true);
+    myLibrary.addBook("Plato", "Republic", 206, false);
 
-    const cardsContainer = document.createElement("div");
-    cardsContainer.classList.add("cards-container");
-    main.appendChild(cardsContainer);
-
-    for (let i = 0; i < myLibrary.length; i++) {
-      cardsContainer.appendChild(prepareCard(myLibrary[i], i));
-    }
+    myLibrary.books.forEach((book, idx) => {
+      cardsContainer.appendChild(prepareCard(book, idx));
+    });
   }
 
   function removeBook() {
     const idx = this.dataset.bookIndex;
-    myLibrary.splice(idx, 1);
-
+    myLibrary.removeBook(idx);
     this.parentNode.remove();
-    // Update book-index attribute when removing.
-    const removeButtons = document.querySelectorAll(".remove");
-    for (let i = idx; i < myLibrary.length; i++) {
-      removeButtons[i].dataset.bookIndex =
-        parseInt(removeButtons[i].dataset.bookIndex) - 1;
-    }
+
+    // When removing a book, shift the remaining indices to the left
+    Array.from(removeButtons)
+      .slice(idx)
+      .forEach((removeButton) => {
+        removeButton.dataset.bookIndex =
+          parseInt(removeButton.dataset.bookIndex) - 1;
+      });
   }
 
-  const button = document.querySelector(".new-book-btn");
+  const removeButtons = document.querySelectorAll(".card-remove");
+  const form = document.querySelector(".form");
+  const cardsContainer = document.querySelector(".cards-container");
+  const addBookButton = document.querySelector(".add-book-btn");
   const modalContainer = document.querySelector(".modal-container");
   const modal = document.querySelector(".modal");
-  const main = document.getElementsByTagName("main")[0];
+  const submitButton = document.querySelector(".submit");
 
-  button.addEventListener("click", () => {
+  submitButton.addEventListener("click", addBook);
+
+  addBookButton.addEventListener("click", () => {
     modalContainer.classList.add("show");
   });
 
   document.addEventListener("click", (event) => {
-    if (event.target === button || modal.contains(event.target)) {
+    if (event.target === addBookButton || modal.contains(event.target)) {
       return;
     } else if (modalContainer.classList.contains("show")) {
       modalContainer.classList.remove("show");
     }
   });
 
-  let myLibrary = [];
+  const myLibrary = new Library([]);
   prepareCards();
 })();
